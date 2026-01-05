67fc7b9a43e44a22ba045447f45956507cf70ac3
Zoushui <zoushui@outlook.com>
2026-01-05 20:31:24 +0800

    feat(motor):实现了双臂同步运动测试例

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include "HAL.h"
 
+#define MOTOR_PWM_DUTY 0.7f  // 电机PWM占空比
+#define MOTOR_RUN_TIME 300   // 电机运行时间，单位ms
 
 //打印信息，测试用
 void print_task(void *pvParameters)
@@ -8,14 +10,39 @@ void print_task(void *pvParameters)
     while(1)
     {
         float cur = current_read();
-        Serial.print(cur, 4); // 保留4位小数
+        Serial.print(cur, 4); 
         Serial.print(",");
         Serial.print(angleA_read(), 2);
         Serial.print(",");
         Serial.println(angleB_read(), 2);
 
-        vTaskDelay(pdMS_TO_TICKS(100));
+        delay(10);
     }
+}
+
+//测试夹爪同步运行
+void gripper_sync_run(float base_duty, int total_ms)
+{
+    unsigned long start = millis();
+
+    while (millis() - start < total_ms)
+    {
+        float a = angleA_read();
+        float b = angleB_read();
+
+        float diff = a - b;
+        float sync = -0.01 * diff;
+
+        float dutyA = constrain(base_duty + sync, -1.0f, 1.0f);
+        float dutyB = constrain(base_duty - sync, -1.0f, 1.0f);
+
+        motorA_set_pwm(dutyA);
+        motorB_set_pwm(dutyB);
+
+        delay(1); //这里必须有个延时不然运行时要报错，服了  
+    }
+
+    motor_stop();   // 结束时停下
 }
 
 
@@ -48,31 +75,41 @@ void loop()
 
     if (msg == "A1")
     {
-      motorA_set_pwm(0.8);
-      delay(50);
+      motorA_set_pwm(MOTOR_PWM_DUTY);
+      delay(MOTOR_RUN_TIME);
       motor_stop();
       Serial.println("A1高电平50ms执行完毕");
     }
     else if (msg == "A2")
     {
-      motorA_set_pwm(-0.8);
-      delay(50);
+      motorA_set_pwm(-MOTOR_PWM_DUTY);
+      delay(MOTOR_RUN_TIME);
       motor_stop();
       Serial.println("A2高电平50ms执行完毕");
     }
     else if (msg == "B1")
     {
-      motorB_set_pwm(0.8);
-      delay(50);
+      motorB_set_pwm(MOTOR_PWM_DUTY);
+      delay(MOTOR_RUN_TIME);
       motor_stop();
-      Serial.println("B1高电平50ms执行完毕");
+      Serial.println("B1高电平执行完毕");
     }
     else if (msg == "B2")
     {
-      motorB_set_pwm(-0.8);
-      delay(50);
+      motorB_set_pwm(-MOTOR_PWM_DUTY);
+      delay(MOTOR_RUN_TIME);
       motor_stop();
-      Serial.println("B2高电平50ms执行完毕");
+      Serial.println("B2高电平执行完毕");
+    }
+    else if (msg == "M1")
+    {
+      gripper_sync_run(MOTOR_PWM_DUTY, MOTOR_RUN_TIME);
+      Serial.println("夹紧执行完毕");
+    }
+    else if (msg == "M2")
+    {
+      gripper_sync_run(-MOTOR_PWM_DUTY, MOTOR_RUN_TIME);
+      Serial.println("放松执行完毕");
     }
     else
     {
